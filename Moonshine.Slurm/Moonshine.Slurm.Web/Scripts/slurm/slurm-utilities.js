e0657203a99b8965d92a5acc21ce3a779025d46a
Timothy Brandt <tim.wilk.brandt@gmail.com>
2016-08-04 15:30:18 -0400

    fleshing out js

--- Moonshine.Slurm/Moonshine.Slurm.Web/Scripts/slurm/slurm-utilities.js
+++ Moonshine.Slurm/Moonshine.Slurm.Web/Scripts/slurm/slurm-utilities.js
@@ -1,19 +1,38 @@
 ﻿var slurm = new slurm || {};
 
-slurm.utiliies = {
+slurm.utilities = {
 
     Settings: {
-        fbOauthToken: null
+        fbOauthToken: null,
+        views: ['#home', '#step1', '#step2', '#step3', '#step4', '#trending', '#facebook-login', '#event-excuses']
     },
 
     ShowContainer: function (selector) {
 
-        $(selector).removeClass('hidden');
+        if ($(selector).hasClass('hidden')) {
+
+            $(selector).removeClass('hidden');
+        }
+
     },
 
     HideContainer: function (selector) {
 
-        $(selector).addClass('hidden');
+        if (!$(selector).hasClass('hidden')) {
 
+            $(selector).addClass('hidden');
+        }
+    },
+
+    ChangeViews: function (selector) {
+
+        $.each(slurm.utilities.Settings.views, function (index, view) {
+            if (!$(view).hasClass('hidden')) {
+                $(view).fadeOut('fast', function () {
+                    $(selector).fadeIn('slow', function () { });
+                });
+            }
+        });
     }
+
 };
--- Moonshine.Slurm/Moonshine.Slurm.Web/Scripts/slurm/slurm.js
+++ Moonshine.Slurm/Moonshine.Slurm.Web/Scripts/slurm/slurm.js
@@ -1,22 +1,57 @@
-﻿var slurm = new slurm || {};
+﻿/// <reference path="slurm-utilities.js" />
+
+var slurm = new slurm || {};
 
 slurm.main = {
-    
+    util: slurm.utilities || {},
+    completedSteps: [],
+
     init: function () { },
 
-    ShowStep1: function () { },
+    returnToSteps: function () {
+        
+        var id = '#step1';
 
-    ShowStep2: function () { },
+        if(slurm.main.completedSteps.length > 0) {
+            id = slurm.main.completedSteps[slurm.main.completedSteps.length - 1];
+        }
+        
+        slurm.main.changeViews(id);
 
-    ShowStep3: function () { },
+    },
 
-    ShowStep4: function () { },
+    returnHome: function () {
 
-    ShowRosettaExcuses: function () { },
+        slurm.main.completedSteps = [];
+        slurm.main.changeViews('#home');
 
-    ShowTrendingExcuses: function () { },
+    },
 
-    ShowFacebookLogin: function () {},
+    changeViews: function (id) {
+
+        slurm.main.util.changeViews(id);
+
+    },
+
+    ShowStep1: function () { slurm.main.changeViews('#step1'); slurm.main.completedSteps = [];},
+
+    ShowStep2: function () { slurm.main.changeViews('#step2'); slurm.main.completedSteps.push('#step1'); },
+
+    ShowStep3: function () { slurm.main.changeViews('#step3'); slurm.main.completedSteps.push('#step2'); },
+
+    ShowStep4: function () { slurm.main.changeViews('#step4'); slurm.main.completedSteps.push('#step3'); },
+
+    ShowRosettaExcuses: function () { slurm.main.changeViews('#rosetta-excuses'); },
+
+    ShowTrendingExcuses: function () { slurm.main.changeViews('#trending-excuses'); },
+
+    ShowFacebookLogin: function () { slurm.main.changeViews('#facebook-login'); },
+
+    CloseRosettaExcuses: function () { slurm.main.returnToSteps(); },
+
+    CloseTrendingExcuses: function () { slurm.main.returnToSteps(); },
+
+    CloseFacebookLogin: function () { slurm.main.returnToSteps(); },
 
     FBEventSelected: function () { },
 
@@ -30,7 +65,9 @@ slurm.main = {
 
     TrendingEventTypeClick: function () { },
 
-    GoHomeClick: function () { },
+    GoHomeClick: function () {
+
+    },
   
 };
 
